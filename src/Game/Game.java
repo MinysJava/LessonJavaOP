54f8855b3ff25f31e06128b294361174f0183fb4
MinysJava <poforli@gmail.com>
2019-07-21 16:20:12 +0500

    игра

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -5,9 +5,7 @@ import java.util.Random;
 class Game {
     public static void main(String[] args) {
 
-        Random randomStep = new Random();
-        Random randomHealing = new Random();
-        int round = 3;
+
 
         Hero[] team1 = new Hero[]{new Warrior(250, "Тигрил", 50, 0)
                 , new Assasin(150, "Акали", 70, 0)
@@ -17,23 +15,9 @@ class Game {
                 , new Assasin(160, "Джинкс", 90, 0)
                 , new Doctor(110, "Зои", 0, 80)};
 
-        for (int j = 0; j < round; j++) {
-            for (int i = 0; i < team1.length; i++) {
-                if(randomStep.nextInt(2) == 0) {
-                    if(team1[i] instanceof Doctor) {
-                        team1[i].healing(team1[randomHealing.nextInt(2)]);
-                    } else {
-                        team1[i].hit(team2[i]);
-                    }
-                } else {
-                    if(team2[i] instanceof Doctor) {
-                        team2[i].healing(team2[randomHealing.nextInt(2)]);
-                    } else {
-                        team2[i].hit(team1[i]);
-                    }
-                }
-            }
-        }
+        Battle battle = new Battle(team1, team2);
+
+
 
         System.out.println("---------------");
 
