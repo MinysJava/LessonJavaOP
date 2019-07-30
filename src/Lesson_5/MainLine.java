c116339a52bc31cc5e670a993d28eea37b6a3af2
MinysJava <poforli@gmail.com>
2019-07-30 23:44:05 +0500

    Урок №5

---
+++ src/Lesson_5/MainLine.java
@@ -0,0 +1,16 @@
+package Lesson_5;
+
+import Lesson_5.*;
+
+public class MainLine {
+    public static void main(String[] args)  {
+        Line t1 = new Line();
+        t1.oneLine();
+        t1.manyLine();
+
+    }
+
+
+
+
+}
